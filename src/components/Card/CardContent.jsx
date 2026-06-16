b8a8cc745d3780fbc69a113ed499e8b8dad6073e
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-16 16:56:02 -0300

    feat: adicionar novo componente CardContent.

--- src/components/Card/CardContent.jsx
+++ src/components/Card/CardContent.jsx
@@ -1,6 +1,6 @@
 export function CardContent({children, layout = "row"}) {
     return (
-        <div className={`card__content layout--${layout}`}>
+        <div className={`card__content  layout--${layout}`}>
             {children}
         </div>
     )
