5b91954feef3473a0c7c58240bc2dfb3e9233195
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-16 16:54:49 -0300

    fix: arrumar importações do DashboardLayout

--- src/app/layout/DashboardLayout.jsx
+++ src/app/layout/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Menu from "../../components/Menu";
-import MenuItem from "../../components/MenuItem";
+import Menu from "../../components/Menu/Menu";
+import MenuItem from "../../components/Menu/MenuItem";
 
 import '../../styles/dashboard.css'
 
