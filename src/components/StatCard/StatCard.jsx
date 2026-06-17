b27c45061e656291cc9a078284dcc032b752ba7b
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-17 16:57:57 -0300

    feat: adicionar card de status.

---
+++ src/components/StatCard/StatCard.jsx
@@ -0,0 +1,18 @@
+import { Card } from "../Card/Card";
+
+export function StatCard({
+    label="Tempo de estudo",
+    value="12h 45m",
+    description="Esta semana"
+}) {
+
+    return (
+        <Card>
+            <span className="card__label">{label}</span>
+            <h3 id="tempo" className="card__value">
+                {value}
+            </h3>
+            <p className="card__description card__description--stat">{description}</p>
+        </Card>
+    )
+}
