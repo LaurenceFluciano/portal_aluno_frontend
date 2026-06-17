4500fd6fc731d6394001432a06fcc702ff656662
laurencefl <laurencefluciano@gmail.com>
2026-06-17 20:04:28 -0300

    adicionar busca de dados na pagina de disciplina

--- src/app/pages/dashboard/Discipline.jsx
+++ src/app/pages/dashboard/Discipline.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
 import { DisciplineCard } from "../../../components/DisciplineCard/DisciplineCard";
 
 export default function Discipline() {
+    const [disciplinas, setDisciplinas] = useState([])
+    const [erro, setErro] = useState(false)
+
+
+    useEffect(() => {
+        async function loadData() {
+            try {
+                const response = await fetch("../../../mock/discipline.json");
+
+                if (!response.ok) {
+                    setError(true);
+                    return;
+                }
+
+                const data = await response.json();
+                setDisciplinas(data.response);
+            } catch (error) {
+                setError(true);
+            }
+        }
+
+        loadData();
+    })
+
     return (
         <div className="dashboard__container">
             <h2>Minhas Disciplinas</h2>
             <div className="grid sd:col--2 md:col--3 gap--16">
-                <DisciplineCard />
+                {erro && <div>Ocorreu um erro ao buscar os dados</div>}
+                {disciplinas.length > 0 && disciplinas.map((value, index) => (
+                    <DisciplineCard 
+                        key={index}
+                        subject={value.subject}
+                        status={value.status}
+                        professor={value.professor}
+                        progress={value.progress}
+                    />
+                ))}
             </div>
         </div>
     )
