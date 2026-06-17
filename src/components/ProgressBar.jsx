e5ec30741b44be53af62f3c3305684ea8a23271c
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-17 16:57:32 -0300

    feat: adicionar componente de barra de progresso.

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,22 @@
+import { Group } from "./Group";
+
+
+export default function ProgressBar({ progress }) {
+
+    const normalizedProgress = Math.min(Math.max(progress, 0), 100);
+
+    return (
+        <Group className={"align--middle-start"} layout="col">
+            <div 
+                className="card__progress-bar"
+                role="progressbar"
+                aria-valuenow={progress}
+                aria-valuemin={0}
+                aria-valuemax={100}
+            >
+                <div style={{width: `${normalizedProgress}%`}}></div>
+            </div>
+            <div className="card__progress-text">{normalizedProgress}%</div>
+        </Group>
+    )
+}
